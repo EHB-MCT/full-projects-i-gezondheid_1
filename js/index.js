eed224ef0776f151e986439ff398e17f5b3dd933
Zani Dobruna <zani.dobruna@student.ehb.be>
2022-01-19 12:14:01 +0100

    commit

--- js/index.js
+++ js/index.js
@@ -1,12 +1,11 @@
 "use strict";
 
-myNav();
-
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myNav() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-        x.style.display = "none";
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
     } else {
-        x.style.display = "block";
+      x.className = "topnav";
     }
-}
+  }
