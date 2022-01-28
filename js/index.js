63da21066d6333c5895f6b8e26b9c9269dc392ac
Zani Dobruna <zani.dobruna@student.ehb.be>
2022-01-28 01:51:07 +0100

    New design week 2

--- js/index.js
+++ js/index.js
@@ -9,3 +9,29 @@
 //       x.className = "topnav";
 //     }
 //   }
+
+document.getElementById('video-player').controls = false;
+
+///////////////////////////
+// Go back to top BUTTON //
+///////////////////////////
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+    
+// When the user scrolls down 200px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
