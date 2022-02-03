e0fb250eba3701a3573e6c44d27966eb6cecd300
Zani Dobruna <zani.dobruna@student.ehb.be>
2022-02-04 00:22:44 +0100

    commitzani

---
+++ js/index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+//////////////////////////////////
+/// Overlay navigation mobile ///
+////////////////////////////////
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+
+
+/////////////////////////////
+/// video home animation ///
+///////////////////////////
+document.getElementById('video-player').controls = false;
